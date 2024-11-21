7b9fe7b2749c9f1044367a194206788ec1217ac7
tkmai02 <mamemai0611@yahoo.co.jp>
2024-11-22 06:51:47 +0900

    環境変数が設定されていない場合に備えて、エラーメッセージを出力するコードを追加

--- project/src/supabase.js
+++ project/src/supabase.js
@@ -3,6 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabaseの環境変数がありません");
+  }  
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
