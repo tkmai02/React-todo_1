ab8fd4d827df986d46ff7257ed0e826c9f091ea8
tkmai02 <mamemai0611@yahoo.co.jp>
2024-11-22 05:49:59 +0900

    `TodoList.tsx`にて、`filteredTodos`が`undefined`である場合に空配列をデフォルト値として使用するように変更する

--- project/src/components/TodoList.tsx
+++ project/src/components/TodoList.tsx
@@ -1,18 +1,15 @@
 //TODOリストを表示するコンポーネント
 // TodoList.tsx
 import React from "react";
-import { TodoContext } from "../context/TodoContext";
 import TodoItem from "./TodoItem";
 
-function TodoList() {
-  // ContextからフィルタリングされたTODOリストを取得
-  const { filteredTodos } = React.useContext<any>(TodoContext);
-
+function TodoList({ filteredTodos }: { filteredTodos: any[] }) {
   return (
     <ul className="space-y-4">
-      {filteredTodos.map(function (todo: any) {
-        return <TodoItem key={todo.id} todo={todo} />;
-      })}
+      {Array.isArray(filteredTodos) &&
+        filteredTodos.map(function (todo: any) {
+          return <TodoItem key={todo.id} todo={todo} />;
+        })}
     </ul>
   );
 }
